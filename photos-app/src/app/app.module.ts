6ad19a3fc22222725f5c088ee563c2f7953cb2e2
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2023-03-02 23:43:37 -0500

    add design and capacitor

--- photos-app/src/app/app.module.ts
+++ photos-app/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    GalleryComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    IonicModule.forRoot()
-  ],
+  imports: [BrowserModule, AppRoutingModule, IonicModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
