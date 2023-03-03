7932e43746679ff0735c3cd3762f7c4408aeee59
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2023-03-03 00:55:44 -0500

    add remaning of project camera

--- photos-app/src/app/pages/gallery/gallery.component.ts
+++ photos-app/src/app/pages/gallery/gallery.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { PhotosService } from 'src/app/services/photos.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent {
+  photos: string[] = [];
 
+  constructor(private photoService: PhotosService) {
+    this.photos = this.photoService.photos;
+  }
+
+  async takePhoto() {
+    await this.photoService.addNewPhoto();
+  }
 }
